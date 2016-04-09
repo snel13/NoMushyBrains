6b35e8b8ff2551d2ea3046135a8819a39b7f682e
Sam <nelsons3@onid.oregonstate.edu>
2016-04-09 14:12:25 -0400

    fixed?

---
+++ Assets/Scripts/Controller2D.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+    BoxCollider2D collider;
+    
+    void Start() {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour {
+    public Controller2D controller;
+    void Start() {
+        controller = GetComponent<Controller2D> ();
+    }
+}
