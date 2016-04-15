4dfc5aea07f0f25f316d5a890ea22a89738c7a6c
Kara Franco <kara.a.franco@gmail.com>
2016-04-14 20:09:45 -0400

    documentation?

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -9,21 +9,24 @@ public class Controller2D : MonoBehaviour {
     // skin width is the padding around the game object where the rays emit from
     // padding is inset so object can rest on platform 
     const float skinWidth= .015f;
-    
+    // number of rays emitting from player
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
    
     float horizontalRaySpacing;
     float verticalRaySpacing;
-    
+    // class BoxCollider2D is a collider for 2D physics representing an axis-aligned rectangle
     BoxCollider2D collider;
+    // custom struct that holds the origin values for the player rayCast
     RaycastOrigins raycastOrigins;
     
+    // Monobehaviour Start is called on the frame when a script is enabled, before any update methods are called
     void Start() {
         collider = GetComponent<BoxCollider2D>();
         CalculateRaySpacing();
     }
      
+    // use Move function to keep track of the ray casts
     public void Move(Vector3 velocity){
         UpdateRaycastOrigins();
         if (velocity.x != 0)
@@ -38,9 +41,8 @@ public class Controller2D : MonoBehaviour {
 
     }
 
-
-    void HorizontalCollisions(ref Vector3 velocity)
-    {
+    // HorizontalCollisions function uses the RaycastHit2D struct to detect objects in the raycast area (right to left)
+    void HorizontalCollisions(ref Vector3 velocity) {
         float directionX = Mathf.Sign(velocity.x);
         // make sure length is a positive number
         float rayLength = Mathf.Abs(velocity.x) + skinWidth;
@@ -50,6 +52,7 @@ public class Controller2D : MonoBehaviour {
             Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
             rayOrigin += Vector2.up * (horizontalRaySpacing * i);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+            // used for debugging, draws the rays in red
             Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
             if (hit)
             {
@@ -59,7 +62,7 @@ public class Controller2D : MonoBehaviour {
         }
     }
 
-
+    // VerticalCollisions function uses the RaycastHit2D struct to detect objects in the raycast area (bottom to top)
     void VerticalCollisions(ref Vector3 velocity){
         float directionY = Mathf.Sign(velocity.y);
         // make sure length is a positive number
@@ -69,7 +72,7 @@ public class Controller2D : MonoBehaviour {
             Vector2 rayOrigin = (directionY == -1)?raycastOrigins.bottomLeft:raycastOrigins.topLeft;
             rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
-
+            // used for debugging, draws the rays in red
             Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
             if (hit){ 
                 velocity.y = (hit.distance - skinWidth) * directionY;
@@ -78,20 +81,26 @@ public class Controller2D : MonoBehaviour {
         }
     }
 
-    void UpdateRaycastOrigins(){
+    // UpdateRaycastOrigins function is used in Move() function to detect space around player
+    void UpdateRaycastOrigins() {
+        // Bounds struct represents an axis aligned bounding box, this box "overlays" the player
+        // Expandincreases the size by a given amount along  each side
         Bounds bounds = collider.bounds;
         bounds.Expand(skinWidth * -2);
-        
+        // set the raycastOrigins struct's to the bounds values
         raycastOrigins.bottomLeft = new Vector2(bounds.min.x,bounds.min.y);
         raycastOrigins.bottomRight = new Vector2(bounds.max.x,bounds.min.y);
         raycastOrigins.topLeft = new Vector2(bounds.min.x,bounds.max.y);
         raycastOrigins.topRight = new Vector2(bounds.max.x,bounds.max.y);
     }
     
+    // CalculateRaySpacing function is used in Move() function to determine ray spacing
     void CalculateRaySpacing() {
+        // once again set up Bounds struct to represent the bounding box
         Bounds bounds = collider.bounds;
         bounds.Expand (skinWidth * -2);
-        
+        // use Clamp() to clamp a value between min and max 
+        // clamp the value of horizontalRayCount/verticalRayCount between 2 and 2,147,483,647
         horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
         verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
         
@@ -100,10 +109,10 @@ public class Controller2D : MonoBehaviour {
         
     }
     
+    // struct used in UpdateRayCastOrigins to set the bounds to the player object
     struct RaycastOrigins{
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
-        
-
+       
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,22 +3,26 @@ using System.Collections;
 
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
+    
     float moveSpeed = 6;
     float gravity = -20;
+    // use struct to pass positions and directions around 
     Vector3 velocity;
+
 
     Controller2D controller;
     void Start() {
         controller = GetComponent<Controller2D> ();
     }
 
-    void Update()
-    {
+    void Update() {
+        // use struct to represent 2D positions and vectors
+        // Input is the class to read the axes from the keyboard, multi-touch (mobile) or mouse
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         velocity.x = input.x * moveSpeed;
+        // Time is the class that gets the time information
+        // deltaTime is the time in seconds it took to complete the last frame (read only) 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-
     }
 }
