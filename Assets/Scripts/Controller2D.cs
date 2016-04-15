adfae65bc45adb5c4b1a566902c7c14f7e7d1726
rostun <rosa.m.tung@gmail.com>
2016-04-15 13:16:58 -0400

    ?????

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -12,7 +12,11 @@ public class Controller2D : MonoBehaviour {
     // number of rays emitting from player
     public int horizontalRayCount = 4;
     public int verticalRayCount = 4;
-   
+	
+	float maxClimbAngle = 80;
+	
+    public CollisionInfo collisions; //public reference to our collision info
+	
     float horizontalRaySpacing;
     float verticalRaySpacing;
     // class BoxCollider2D is a collider for 2D physics representing an axis-aligned rectangle
@@ -20,6 +24,7 @@ public class Controller2D : MonoBehaviour {
     // custom struct that holds the origin values for the player rayCast
     RaycastOrigins raycastOrigins;
     
+	
     // Monobehaviour Start is called on the frame when a script is enabled, before any update methods are called
     void Start() {
         collider = GetComponent<BoxCollider2D>();
@@ -29,12 +34,11 @@ public class Controller2D : MonoBehaviour {
     // use Move function to keep track of the ray casts
     public void Move(Vector3 velocity){
         UpdateRaycastOrigins();
-        if (velocity.x != 0)
-        {
+		collisions.Reset(); //blank slate each time
+        if (velocity.x != 0){
             HorizontalCollisions(ref velocity);
         }
-        if (velocity.y != 0)
-        {
+        if (velocity.y != 0){
             VerticalCollisions(ref velocity);
         }
         transform.Translate(velocity);
@@ -46,18 +50,38 @@ public class Controller2D : MonoBehaviour {
         float directionX = Mathf.Sign(velocity.x);
         // make sure length is a positive number
         float rayLength = Mathf.Abs(velocity.x) + skinWidth;
-        for (int i = 0; i < horizontalRayCount; i++)
-        {
+        for (int i = 0; i < horizontalRayCount; i++){
             // see what direction we are moving in
             Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
             rayOrigin += Vector2.up * (horizontalRaySpacing * i);
             RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
             // used for debugging, draws the rays in red
             Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
-            if (hit)
-            {
-                velocity.x = (hit.distance - skinWidth) * directionX;
-                rayLength = hit.distance;
+            if (hit){
+				//angled movement
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+				if(i == 0 && slopeAngle <= maxClimbAngle){
+					float distanceToSlopeStart = 0;
+					//starting to climb a new slope 
+					if(slopeAngle != collisions.slopeAngleOld){
+						distanceToSlopeStart = hit.distance-skinWidth;
+						velocity.x -= distanceToSlopeStart * directionX; //only uses velocity x that it has once it reaches the slope
+					}
+					ClimbSlope(ref velocity, slopeAngle);//print(slopeAngle);
+					velocity.x += distanceToSlopeStart * directionX; //add back on
+				}
+				//check other rays if we're not climbing slope
+				if(!collisions.climbingSlope || slopeAngle > maxClimbAngle){
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					rayLength = hit.distance;
+					//update velocity on y axis
+					if(collisions.climbingSlope){
+						velocity.y = Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+					}
+					//set depending on the direction
+					collisions.left = directionX == -1; //if we've hit something and we're going left then this will be true
+					collisions.right = directionX == 1;					
+				}
             }
         }
     }
@@ -77,10 +101,29 @@ public class Controller2D : MonoBehaviour {
             if (hit){ 
                 velocity.y = (hit.distance - skinWidth) * directionY;
                 rayLength = hit.distance;
+				
+				if(collisions.climbingSlope){
+					velocity.x = velocity.y / Mathf.Tan(collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign(velocity.x);
+				}
+				collisions.below = directionY == -1; 
+				collisions.above = directionY == 1;
             }
         }
     }
 
+	void ClimbSlope(ref Vector3 velocity, float slopeAngle){
+		//figure out new velocity x and y for slopes 
+		float moveDistance = Mathf.Abs(velocity.x);
+		float climbVelocityY = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * moveDistance;
+		if(velocity.y <= climbVelocityY){
+			velocity.y = climbVelocityY;
+			velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+			collisions.below = true; //fix jumping	
+			collisions.climbingSlope = true; //set to true
+			collisions.slopeAngle = slopeAngle;
+		}
+	}
+	
     // UpdateRaycastOrigins function is used in Move() function to detect space around player
     void UpdateRaycastOrigins() {
         // Bounds struct represents an axis aligned bounding box, this box "overlays" the player
@@ -114,5 +157,22 @@ public class Controller2D : MonoBehaviour {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
+	
+	public struct CollisionInfo{
+		public bool above, below;
+		public bool left, right;
+		
+		public bool climbingSlope;
+		public float slopeAngle, slopeAngleOld;
+		
+		//reset bools to false
+		public void Reset(){
+			above = below = false;
+			left = right = false;
+			climbingSlope = false;
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
+		}
+	}
        
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,44 @@ using System.Collections;
 [RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
     
+	//affect gravity and jump velocity
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f; //.4 of a second
+	float accelerationTimeAirborne = .2f;
+	float accelerationTimeGrounded = .1f;
     float moveSpeed = 6;
-    float gravity = -20;
+    
+	float gravity;
+	float jumpVelocity;
     // use struct to pass positions and directions around 
     Vector3 velocity;
-
+	float velocityXSmoothing; //for x movement
 
     Controller2D controller;
     void Start() {
         controller = GetComponent<Controller2D> ();
+		//alter physics
+		gravity = -(2*jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+		print("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
     }
 
     void Update() {
+		if(controller.collisions.above || controller.collisions.below){ //fall off more elegantly
+			//reset velocity on the y axis
+			velocity.y = 0;
+		}
         // use struct to represent 2D positions and vectors
         // Input is the class to read the axes from the keyboard, multi-touch (mobile) or mouse
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        velocity.x = input.x * moveSpeed;
+		
+		//if we're standing on something and player hits space, we jump
+		if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below){
+			velocity.y = jumpVelocity;
+		}
+        float targetVelocityX = input.x * moveSpeed;
+		//SmoothDamp function lookup
+		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
         // Time is the class that gets the time information
         // deltaTime is the time in seconds it took to complete the last frame (read only) 
         velocity.y += gravity * Time.deltaTime;
