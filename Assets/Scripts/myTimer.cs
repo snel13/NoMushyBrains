a94547ea80827a76a627bed3a6e6ae4b2feffaee
rostun <rosa.m.tung@gmail.com>
2016-04-29 12:04:54 -0400

    timer

---
+++ Assets/Scripts/myTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; //public Text timerText
+
+public class myTimer : MonoBehaviour {
+	
+	public float myCoolTimer = 99;
+	private Text timerText;
+
+	float minutes;
+	float seconds;
+	string format;
+
+	// Use this for initialization
+	void Start () {
+		timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myCoolTimer -= Time.deltaTime; //+= to count up
+		minutes = Mathf.FloorToInt(myCoolTimer/60);
+		seconds = myCoolTimer - minutes*60;
+		format = string.Format("{0:0}:{1:00}", minutes, seconds); //H:M:SSSS
+		timerText.text = "Timer: " + format;
+	}
+}
